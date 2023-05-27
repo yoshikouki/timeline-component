6cf8b7e8e6f4d09a9c5d2ddd1691f42c5093a630
yoshikouki <yoshikouki@gmail.com>
2023-05-27 19:19:33 +0900

    Create Timeline component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import Image from 'next/image'
+import Timeline from './Timeline'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Timeline />
     </main>
   )
 }
